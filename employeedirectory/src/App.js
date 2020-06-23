235ffa80c614e19cf799dc9fef944d13ae0367e6
Emily Gaska <emilygaska@gmail.com>
2020-06-23 17:05:04 -0500

    Set up the outline for the search input

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from "./components/Header"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
+
+
   return (
     <div className="App">
-    <Header></Header>
+      <Header></Header>
+    </div>
+  );
+}
+
+function SearchName() {
+  return (
+    <div className="searchArea">
+      <div className="input-group">
+        <span>Search</span>
+      </div>
+      <input
+        type="text"
+        className="form-control"
+        id="searchInput"
+        placeholder="Search"
+        onChange = {}
+      
+      />
     </div>
   );
 }
